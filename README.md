bd1e87d68b882b91420ce7e866265700145467c2
David Gouveia <davidluzgouveia@gmail.com>
2018-03-15 02:04:06 +0000

    First and fully functional version

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# ct-fmv
-Encodes or decodes the FMV files on Chrono Trigger for Steam
+Chrono Trigger (Steam) FMV Encoder / Decoder
+============================================
+
+A tool for the Steam version of Chrono Trigger that allows you to decode the FMV files (001-008.dat on the root) and encode your own mp4 files to replace them.
+
+Thanks to [Aemony](http://steamcommunity.com/id/aemony) who posted on the Steam forums showing how easy the format was to decode.
+
+How to use
+----------
+
+- Grab the [latest release from here](https://github.com/davidluzgouveia/ct-fmv/releases)
+- Drag a `.dat` on top of `ct-fmv.exe` to decode it into `.mp4`
+- Drag a `.mp4` on top of `ct-fmv.exe` to encode it into `.dat`
+
+Versions
+--------
+
+- v1.0: Already works both ways and was tested with all of the FMVs
---
+++ ct-fmv.cs
@@ -0,0 +1,79 @@
+using System;
+using System.IO;
+
+public static class ChronoTriggerFmv
+{
+    public static void Main(string[] args)
+    {
+        var defaultColor = Console.ForegroundColor;
+
+        try
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Chrono Trigger (Steam) FMV Encoder / Decoder\n");
+
+            Console.ForegroundColor = defaultColor;
+            Console.WriteLine(
+                "A tool that encodes or decodes the FMV files in the game (the 001-008.dat on the game root)\n");
+
+            if (args.Length != 1)
+            {
+                throw new Exception("Drag a .dat or .mp4 file into this executable");
+            }
+
+            var inputPath = args[0];
+
+            if (!File.Exists(inputPath))
+            {
+                throw new Exception($"{inputPath} is not a file");
+            }
+
+            var isDat = inputPath.EndsWith(".dat");
+            var isMp4 = inputPath.EndsWith(".mp4");
+            if (!isDat && !isMp4)
+            {
+                throw new Exception($"{inputPath} is not a .dat nor a .mp4 file");
+            }
+
+            var outputPath =
+                isDat ? inputPath.Replace(".dat", ".decoded.mp4") : inputPath.Replace(".mp4", ".encoded.dat");
+
+            if (File.Exists(outputPath))
+            {
+                throw new Exception($"There's already a file at {outputPath}, remove it first");
+            }
+
+            var before = File.ReadAllBytes(inputPath);
+            var after = Decode(before);
+
+            File.WriteAllBytes(outputPath, after);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Converted {inputPath} into {outputPath}\n");
+        }
+        catch (Exception e)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(e.Message + "\n");
+        }
+        finally
+        {
+            Console.ForegroundColor = defaultColor;
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+
+    private static byte[] Decode(byte[] input)
+    {
+        var output = new byte[input.Length];
+        var counter = 0xFF;
+        for (var i = 0; i < input.Length; i++)
+        {
+            output[i] = (byte)(input[i] ^ counter);
+            counter = counter == 0 ? 0xFF : counter - 1;
+        }
+
+        return output;
+    }
+}
